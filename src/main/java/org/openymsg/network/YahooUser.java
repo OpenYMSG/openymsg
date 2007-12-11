f7f9d2564d53b298bbcb86ac7f579f8157daf6d7
guusdk <guusdk@cc2fca57-485c-0410-99ba-8c215222810c>
2007-12-11 16:26:37 +0000

    Simplified the generated hashCode() method. This should compute a little faster.

--- src/main/java/org/openymsg/network/YahooUser.java
+++ src/main/java/org/openymsg/network/YahooUser.java
@@ -360,10 +360,7 @@ public class YahooUser {
 	 */
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((userId == null) ? 0 : userId.hashCode());
-		return result;
+		return (userId == null) ? 1 : userId.hashCode();
 	}
 
 	/*
