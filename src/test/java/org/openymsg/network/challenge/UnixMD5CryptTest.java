7971659818a28e339ba5cb71ce6ce8a6fb90719b
guusdk <guusdk@cc2fca57-485c-0410-99ba-8c215222810c>
2007-12-12 15:34:40 +0000

    Added some new testcases for the challenge mechanism.

---
+++ src/test/java/org/openymsg/network/challenge/UnixMD5CryptTest.java
@@ -0,0 +1,20 @@
+package org.openymsg.network.challenge;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Guus der Kinderen, guus@nimbuzz.com
+ *
+ */
+public class UnixMD5CryptTest {
+
+	/**
+	 * Test method for {@link org.openymsg.network.challenge.UnixCrypt#md5Crypt(java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testMd5CryptStringString() throws Exception {
+		assertEquals("$1$caeiHQwX$hsKqOjrFRRN6K32OWkCBf1$802KwXGXHHXviR1gYFb580" , UnixCrypt.md5Crypt("mykey" , "caeiHQwX$hsKqOjrFRRN6K32OWkCBf1"));
+	}
+}
