452bf3411ecf79f6b0c82d855e83fa7081c29cbb
guusdk <guusdk@cc2fca57-485c-0410-99ba-8c215222810c>
2007-12-13 12:04:38 +0000

    Improved roster tests (YMSG-18)

--- src/test/java/org/openymsg/roster/RosterAdd.java
+++ src/test/java/org/openymsg/roster/RosterAdd.java
@@ -1,6 +1,8 @@
 package org.openymsg.roster;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.openymsg.network.YahooUser;
@@ -27,12 +29,35 @@ public class RosterAdd {
 		final YahooUser user = new YahooUser("user", "group");
 
 		// execution
-		roster.add(user);
+		final boolean result = roster.add(user);
 
 		// validation
+		assertTrue(result);
 		assertEquals("sendNewFriendRequest", manager.getMethod());
 		assertEquals("user", manager.getFriendId());
 		assertEquals("group", manager.getGroupId());
 	}
 
+	/**
+	 * Checks that adding a user to the roster that already exists on the roster
+	 * does not trigger the FriendManager.
+	 */
+	@Test
+	public void testCallingAddTwiceDoesntTriggerFriendManager() {
+		// setup
+		final MockFriendManager manager = new MockFriendManager();
+		final Roster roster = new Roster(manager);
+		final YahooUser user = new YahooUser("user", "group");
+		roster.add(user); // first time
+		manager.reset();
+
+		// execution
+		final boolean result = roster.add(user);
+
+		// validation
+		assertFalse(result);
+		assertEquals("sendNewFriendRequest", manager.getMethod());
+		assertEquals("user", manager.getFriendId());
+		assertEquals("group", manager.getGroupId());
+	}
 }
