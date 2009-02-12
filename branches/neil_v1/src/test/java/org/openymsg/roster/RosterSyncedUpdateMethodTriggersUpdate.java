1fef84b88bbb1663ef361780deddc210e8774682
neilhart <neilhart@cc2fca57-485c-0410-99ba-8c215222810c>
2009-02-12 15:46:46 +0000

    create a branch

---
+++ branches/neil_v1/src/test/java/org/openymsg/roster/RosterSyncedUpdateMethodTriggersUpdate.java
@@ -0,0 +1,100 @@
+package org.openymsg.roster;
+
+import static org.junit.Assert.assertEquals;
+import junitx.util.PrivateAccessor;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openymsg.network.YahooUser;
+
+/**
+ * Testcase to check if an 'syncedUpdate()' to a {@link Roster} triggers the
+ * expected Event to be broadcasted to all listeners.
+ * <p>
+ * Note that this method checks the event listener routines only. Other methods
+ * will check if the 'syncedUpdate' functionality actually manipulate the
+ * content of the Roster.
+ * 
+ * @author Guus der Kinderen, guus@nimbuzz.com
+ */
+public class RosterSyncedUpdateMethodTriggersUpdate {
+
+	private static final YahooUser USER = new YahooUser("dummy");
+	private Roster roster;
+
+	@Before
+	public void setUp() throws Throwable {
+		roster = new Roster(new MockFriendManager());
+		PrivateAccessor.invoke(roster, "syncedAdd",
+				new Class[] { YahooUser.class }, new Object[] { USER });
+	}
+
+	/**
+	 * Checks if a broadcasted event is received by a RosterListener that has
+	 * been added to the Roster.
+	 * 
+	 * @throws Throwable
+	 */
+	@Test
+	public void testReceiveOnOneRegisteredListener() throws Throwable {
+		final MockRosterListener listener = new MockRosterListener();
+		roster.addRosterListener(listener);
+		PrivateAccessor
+				.invoke(roster, "syncedUpdate", new Class[] { String.class,
+						YahooUser.class }, new Object[] { "dummy", USER });
+		assertEquals(1, listener.getEventCount());
+	}
+
+	/**
+	 * Checks if a broadcasted event is received by all RosterListeners that
+	 * have been added to the Roster.
+	 * 
+	 * @throws Throwable
+	 */
+	@Test
+	public void testReceiveOnTwoRegisteredListeners() throws Throwable {
+		final MockRosterListener listenerOne = new MockRosterListener();
+		final MockRosterListener listenerTwo = new MockRosterListener();
+		roster.addRosterListener(listenerOne);
+		roster.addRosterListener(listenerTwo);
+		PrivateAccessor
+				.invoke(roster, "syncedUpdate", new Class[] { String.class,
+						YahooUser.class }, new Object[] { "dummy", USER });
+		assertEquals(1, listenerOne.getEventCount());
+		assertEquals(1, listenerTwo.getEventCount());
+	}
+
+	/**
+	 * Checks if a broadcasted event is disregarded by a RosterListener that has
+	 * not been added to the Roster.
+	 * 
+	 * @throws Throwable
+	 */
+	@Test
+	public void testDontReceiveOnUnregisteredListener() throws Throwable {
+		final MockRosterListener listener = new MockRosterListener();
+		PrivateAccessor
+				.invoke(roster, "syncedUpdate", new Class[] { String.class,
+						YahooUser.class }, new Object[] { "dummy", USER });
+		assertEquals(0, listener.getEventCount());
+	}
+
+	/**
+	 * Checks if an event gets recorded by the registered listener while it does
+	 * not at an unregistered one.
+	 * 
+	 * @throws Throwable
+	 */
+	@Test
+	public void testCombinationOfRegisteredAndUnregisteredListeners()
+			throws Throwable {
+		final MockRosterListener listener = new MockRosterListener();
+		final MockRosterListener nonlistener = new MockRosterListener();
+		roster.addRosterListener(listener);
+		PrivateAccessor
+				.invoke(roster, "syncedUpdate", new Class[] { String.class,
+						YahooUser.class }, new Object[] { "dummy", USER });
+		assertEquals(1, listener.getEventCount());
+		assertEquals(0, nonlistener.getEventCount());
+	}
+}
