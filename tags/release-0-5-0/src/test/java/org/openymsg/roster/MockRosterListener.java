0cd4b03476b8f452add7a064da41554732f9575c
neilhart <neilhart@cc2fca57-485c-0410-99ba-8c215222810c>
2010-05-20 18:20:39 +0000

    release for 0.5.0

---
+++ tags/release-0-5-0/src/test/java/org/openymsg/roster/MockRosterListener.java
@@ -0,0 +1,37 @@
+package org.openymsg.roster;
+
+/**
+ * Utility {@link RosterListener} implementation that lets you count how often a request has been dispatched to it.
+ * 
+ * @author Guus der Kinderen, guus@nimbuzz.com
+ * 
+ */
+public class MockRosterListener implements RosterListener {
+
+    private int eventCount = 0;
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.openymsg.roster.RosterListener#rosterChanged(org.openymsg.roster.RosterEvent)
+     */
+    public void rosterChanged(RosterEvent event) {
+        eventCount++;
+    }
+
+    /**
+     * Returns the number of events that have been received by this listener.
+     * 
+     * @return the eventCount
+     */
+    public int getEventCount() {
+        return eventCount;
+    }
+
+    /**
+     * Sets the event counter to 0.
+     */
+    public void resetEventCount() {
+        eventCount = 0;
+    }
+}
