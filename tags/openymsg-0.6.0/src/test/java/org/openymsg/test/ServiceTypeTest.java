2c47b4dfd5d1c3ecb0601891c2106825ac2df5c1
neilhart <neilhart@cc2fca57-485c-0410-99ba-8c215222810c>
2011-10-10 14:00:48 +0000

    [maven-release-plugin]  copy for tag openymsg-0.6.0

---
+++ tags/openymsg-0.6.0/src/test/java/org/openymsg/test/ServiceTypeTest.java
@@ -0,0 +1,28 @@
+package org.openymsg.test;
+
+import static org.junit.Assert.assertFalse;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+import org.junit.Test;
+import org.openymsg.network.ServiceType;
+
+public class ServiceTypeTest {
+
+    /**
+     * Checks if every ServiceType int value is unique.
+     */
+    @Test
+    public void testGetValue() {
+        final Collection<Integer> checkedValues = new HashSet<Integer>();
+        final ServiceType[] types = ServiceType.values();
+        for (int i = 0; i < types.length; i++) {
+            final Integer serviceTypeIntValue = Integer.valueOf(types[i].getValue());
+            assertFalse("Non-unique ServiceType value " + types[i].getValue(), checkedValues
+                    .contains(serviceTypeIntValue));
+            checkedValues.add(serviceTypeIntValue);
+        }
+    }
+
+}
---
+++ tags/openymsg-0.6.0/src/test/java/org/openymsg/test/YahooUserTest.java
@@ -0,0 +1,45 @@
+/*
+ * OpenYMSG, an implementation of the Yahoo Instant Messaging and Chat protocol.
+ * Copyright (C) 2007 G. der Kinderen, Nimbuzz.com 
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. 
+ */
+package org.openymsg.test;
+
+import junitx.extensions.EqualsHashCodeTestCase;
+
+import org.openymsg.network.YahooUser;
+
+/**
+ * Basic Equality and HashCode contract checks.
+ * 
+ * @author G. der Kinderen, Nimbuzz B.V. guus@nimbuzz.com
+ */
+public class YahooUserTest extends EqualsHashCodeTestCase {
+
+    public YahooUserTest() {
+        super(YahooUserTest.class.getName());
+    }
+
+    @Override
+    protected Object createInstance() throws Exception {
+        return new YahooUser("same");
+    }
+
+    @Override
+    protected Object createNotEqualInstance() throws Exception {
+        return new YahooUser("different");
+    }
+}
