750f830e341dc750b8713bfef6a20184d0f6a096
neilhart <neilhart@cc2fca57-485c-0410-99ba-8c215222810c>
2012-02-29 14:33:40 +0000

    adding more javadoc

---
+++ branches/neil-0.7/src/main/java/org/openymsg/execute/write/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Framework for writing messages to Yahoo.  The interfaces that a developer uses are Message and PacketWriter. 
+ * Each subclass of Message will 
+ * supply the necessary information for the message packet ot Yahoo.  
+ * PacketWriter handles taking that Message and getting it on the Yahoo 
+ * connection.
+ * The implementation of PacketWriter is PacketWriterImpl.  It uses the Dispatcher to executor a job containing
+ * the Message.  A MessageExecuteRequest is what wraps the Messages.
+ *  <img src="doc-files/write.jpg" alt="Overview of Write Framework"/> 
+ */
+package org.openymsg.execute.write;
+
