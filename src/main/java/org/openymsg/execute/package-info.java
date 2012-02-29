51b7e1245ea71c6d7bfa70fc5733e98898c53148
neilhart <neil1hart@users.noreply.github.com>
2012-02-29 14:33:40 +0000

    adding more javadoc

---
+++ src/main/java/org/openymsg/execute/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Execution Framework
+ * The Execution Framework is made of 3 parts: Dispatch, Read and Write.  
+ * Dispatch is the heart of the framework and handles the execution of jobs.  Each job
+ * will be alerted if it has an exception.  Jobs can also be scheduled to reoccur at
+ * regular intervals
+ * Read and Write are the parts that handle the sending and receiving of messages
+ * using the Yahoo protocol.
+ *  <img src="doc-files/execute.jpg" alt="Overview of Execution Framework"/> 
+ */
+package org.openymsg.execute;
+
