dde348a3686781dc343ffb4f1a7807749b368422
neilhart <neilhart@cc2fca57-485c-0410-99ba-8c215222810c>
2012-05-16 16:05:47 +0000

    better handling logout

---
+++ branches/neil-0.7/src/main/java/org/openymsg/execute/dispatch/ScheduleTaskCompletionException.java
@@ -0,0 +1,8 @@
+package org.openymsg.execute.dispatch;
+
+public class ScheduleTaskCompletionException extends RuntimeException {
+
+	/** serial version id */
+	private static final long serialVersionUID = 1L;
+
+}
